b88d7ee5078b826c106087a70665e3cc24a3efd4
hypercubist <qpinguz@gmail.com>
2021-09-18 23:17:37 +0900

    make video search page

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -33,11 +33,32 @@ const fakeUser = {
 ];*/
 
 export const home = async (req, res) => {
-  const videos = await Video.find({});
+  const videos = await Video.find({}).sort({ createdAt: "desc" });
   return res.render("home", { pageTitle: "Home", fakeUser, videos });
 };
-export const search = (req, res) => res.send("search");
-export const upload = (req, res) => res.send("upload video");
+
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video", fakeUser });
+};
+
+export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  try {
+    await Video.create({
+      title,
+      description,
+      hashtags: Video.formatHashtags(hashtags),
+    });
+    return res.redirect("/");
+  } catch (error) {
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      fakeUser,
+      errorMessage: error._message,
+    });
+  }
+};
+
 export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
@@ -50,12 +71,6 @@ export const watch = async (req, res) => {
     video,
   });
 };
-export const deleteVideo = async (req, res) => {
-  const { id } = req.params;
-  await Video.findByIdAndDelete(id);
-  return res.redirect("/");
-};
-res.send("remove video");
 
 export const getEdit = async (req, res) => {
   const { id } = req.params;
@@ -83,24 +98,21 @@ export const postEdit = async (req, res) => {
   return res.redirect(`/video/${id}`);
 };
 
-export const getUpload = (req, res) => {
-  return res.render("upload", { pageTitle: "Upload Video", fakeUser });
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  await Video.findByIdAndDelete(id);
+  return res.redirect("/");
 };
 
-export const postUpload = async (req, res) => {
-  const { title, description, hashtags } = req.body;
-  try {
-    await Video.create({
-      title,
-      description,
-      hashtags: Video.formatHashtags(hashtags),
-    });
-    return res.redirect("/");
-  } catch (error) {
-    return res.render("upload", {
-      pageTitle: "Upload Video",
-      fakeUser,
-      errorMessage: error._message,
+export const search = async (req, res) => {
+  const { keyword } = req.query;
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(keyword, "i"),
+      },
     });
   }
+  return res.render("search", { pageTitle: "Search", fakeUser, videos });
 };
