0b7ea157136e9a58c8027a11490fa2acb64f5d3d
hypercubist <qpinguz@gmail.com>
2021-10-09 18:47:22 +0900

    transcoding video, thumbnail using by webassembly ffmpeg

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,3 +1,5 @@
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
@@ -5,12 +7,71 @@ let stream;
 let recorder;
 let videoFile;
 
-const handleDownload = () => {
+const files = {
+  input: "recording.webm",
+  output: "output.mp4",
+  thumbnail: "thumbnail.jpg",
+};
+
+const downloadFile = (fileUrl, fileName) => {
   const a = document.createElement("a");
-  a.href = videoFile;
-  a.download = "MyRecording.webm";
+  a.href = fileUrl;
+  a.download = fileName;
   document.body.appendChild(a);
   a.click();
+};
+
+const handleDownload = async () => {
+  startBtn.removeEventListener("click", handleDownload);
+  startBtn.innerText = "Transcoding...";
+  startBtn.disabled = true;
+
+  const ffmpeg = createFFmpeg({ log: true });
+  await ffmpeg.load();
+  ffmpeg.FS("writeFile", files.input, await fetchFile(videoFile));
+  // ff fs에 video url을 통해 파일 저장
+
+  await ffmpeg.run("-i", files.input, "-r", "60", files.output);
+  await ffmpeg.run(
+    "-i",
+    files.input,
+    "-ss",
+    "00:00:01",
+    "-frames:v",
+    "1",
+    files.thumbnail
+  );
+  //인코딩 60fps, 스크린샷 1frame , file2개 생성
+
+  const mp4File = ffmpeg.FS("readFile", files.output);
+  const thumbFile = ffmpeg.FS("readFile", files.thumbnail);
+  // 생성된 file읽기
+
+  const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
+  const thumbBlob = new Blob([thumbFile.buffer], { type: image / jpg });
+  // buffer를 통해 blob file 생성
+
+  const mp4Url = URL.createObjectURL(mp4Blob);
+  const thumbUrl = URL.createObjectURL(thumbBlob);
+  // blob으로 가상 url만들기
+
+  downloadFile(mp4Url, "MyRecording.mp4");
+  downloadFile(thumbUrl, "Mythumbnail.jpg");
+  // url만들어서 가상 클릭하여 다운로드 시키기
+
+  ffmpeg.FS("unlink", files.input);
+  ffmpeg.FS("unlink", files.output);
+  ffmpeg.FS("unlink", files.thumbnail);
+  //완성된 file제거
+
+  URL.revokeObjectURL(mp4Url);
+  URL.revokeObjectURL(thumbUrl);
+  URL.revokeObjectURL(videoFile);
+  //사용한 link 제거
+
+  startBtn.disabled = false;
+  startBtn.innerText = "Record Again";
+  startBtn.addEventListener("click", handleStart);
 };
 
 const handleStop = () => {
